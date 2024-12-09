5a2eecde5e0ed15f1792383d648f10a00392e93c
Дмитрий <dmitriy.golovin.2001.03@mail.ru>
2024-12-09 10:35:16 +0300

    задание 4.1

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { TCardsProps } from '../../mocks/offers';
 import Cards from '../../cards/cards';
 import CommentForm from '../../commentForm/commentForm';
+import { Link } from 'react-router-dom';
 export default function Offer({ cards }: TCardsProps): JSX.Element {
   const [cardHover, setCardHover] = useState<string | null>(null);
   const onCommontFormSubmit = (text:string) => {
@@ -13,24 +14,24 @@ export default function Offer({ cards }: TCardsProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
+              <Link className="header__logo-link" to="main.html">
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              </Link>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link className="header__nav-link header__nav-link--profile" to="#">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
+                  <Link className="header__nav-link" to="#">
                     <span className="header__signout">Sign out</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
